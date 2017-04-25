07d66a3864ce34532916beff7be79fc0660272fb
Hack Reactor Student <student@hackreactor.com>
2017-04-24 20:08:01 -0700

    Began parsing cookies

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,27 @@
 const parseCookies = (req, res, next) => {
+
+  var dough = req.headers.Cookies;
+  // if no cookies return empty object
+  if ( !dough ){
+    req.cookies = {};
+  }
+  // parse cookies back to Cookie object
+  else {
+
+    var cookies = {};
+    dough.split(';').forEach(function(cookieball){
+
+      var cut = cookieball.getIndexOf('=');
+
+      // var key = cookieball.match('//');
+      // var val = cookieball.match('//');
+
+      cookies[ cookieball.slice(0,cut) ] = cookieball.slice(match+1);
+    });
+    req.cookies = cookies;
+    console.log("COOKIES = ",req.cookies)
+
+  }
 };
 
 module.exports = parseCookies;
--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -47,7 +47,7 @@ describe('', function() {
     /**************************************************************************************/
     /* TODO: If you create a new MySQL tables, add it to the tablenames collection below. */
     /**************************************************************************************/
-    var tablenames = ['links', 'clicks', 'users'];
+    var tablenames = ['links', 'clicks', 'users', 'sessions'];
 
     db.connect(function(err) {
       if (err) { return done(err); }
